0bd182f499ce25104ce2ee66abde9802ef522c44
Arshlan <arshalanraja@gmail.com>
2024-02-16 21:27:15 +0530

    using the useEffect

---
+++ ReactFolio2/dummy/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import { useEffect, useState } from 'react';
+
+function App() {
+
+  const [text , setText] = useState('');
+
+  useEffect(() =>{
+    console.log("UI rendering done bro...")
+  })
+
+  function changeHandler(event) {
+
+      console.log(text);
+      setText(event.target.value);
+       
+  }
+
+  return (
+    <div className="App">
+     <input type='text' onChange={changeHandler}></input>
+    </div>
+  );
+}
+
+export default App;
