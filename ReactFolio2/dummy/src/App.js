ebf41c6b6e0fd6225dab612eeea37454aa2271fc
Arshlan <arshalanraja@gmail.com>
2024-02-16 21:45:04 +0530

    learn the variation of the useEffect

--- ReactFolio2/dummy/src/App.js
+++ ReactFolio2/dummy/src/App.js
@@ -6,9 +6,37 @@ function App() {
 
   const [text , setText] = useState('');
 
+  // variation 1 -> every render 
+  // useEffect(() =>{
+
+  //   console.log("UI rendering done bro...")
+
+  // });
+
+  // variation 2 -> first render 
+  // useEffect(() =>{
+
+  //   console.log("UI rendering done bro...")
+
+  // },[]);
+
+  // variation 3 -> whenever dependency changes
   useEffect(() =>{
-    console.log("UI rendering done bro...")
-  })
+
+    console.log("Change Observed");
+
+  },[text]);
+
+    // variation 4 -> to handle unmounting of a component
+    useEffect(() =>{
+
+      console.log("listener added");  // uske bad ye chalega 
+
+      return () => {
+        console.log("listener removed");    // pehle ye line chalegi 
+      }
+
+    },[text]);
 
   function changeHandler(event) {
 
